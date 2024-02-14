2c14a7d4c7e521890e22703e2a5ed947a2bb1554
Saath Satheeshkumar <saathsatheesh@gmail.com>
2024-02-14 16:55:33 +0000

    feat: Add notification service crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+
+use notify_rust::{Hint, Timeout, Notification};
 fn main() {
-    println!("Hello, world!");
-}
+Notification::new()
+    .summary("Category:email")
+    .body("This has nothing to do with emails.\nIt should not go away until you acknowledge it.")
+    .icon("thunderbird")
+    .appname("thunderbird")
+    .hint(Hint::Category("email".to_owned()))
+    .hint(Hint::Resident(true)) // this is not supported by all implementations
+    .timeout(Timeout::Never) // this however is
+    .show().unwrap();}
