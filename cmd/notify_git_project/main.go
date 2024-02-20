c1fbb1439ee0f786c191203adedf3577ad83ba25
Saath Satheeshkumar <saathsatheesh@gmail.com>
2024-02-20 21:30:15 +0000

    feat: Create daemon loop with calls to GitHub's API - Create notifications for successful and failed calls to API

--- cmd/notify_git_project/main.go
+++ cmd/notify_git_project/main.go
@@ -1,17 +1,102 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
+	"time"
+
+	"github.com/gen2brain/beeep"
+	"github.com/google/go-github/v59/github"
+	"github.com/joho/godotenv"
 )
 
+func startDaemon(client *github.Client, owner string, repoName string) {
+	lastTimeUpdate := time.Now()
+	<-time.Tick(time.Hour * 1)
+
+	for range time.Tick(time.Hour * 1) {
+		log.Println(fmt.Sprint("Sending "))
+		lastPush, err := callGitHubAPI(client, owner, repoName)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = sendNotification(owner, repoName, lastPush, lastTimeUpdate)
+		if err != nil {
+			log.Fatal(err)
+		}
+		lastTimeUpdate = time.Now()
+	}
+}
+
+// Sends the correct notification according to
+// the last push to the repository and the last time
+// this notification was sent.
+func sendNotification(owner string, repoName string, lastPush time.Time, lastTimeUpdate time.Time) error {
+	compareTimeStamps := lastPush.Compare(lastTimeUpdate)
+	if (compareTimeStamps == 1) || (compareTimeStamps == 0) {
+		log.Println("Repo Updated Notification Sent")
+		err := notifyRepoUpdated(owner, repoName, lastPush)
+		if err != nil {
+			return err
+		}
+		return nil
+	} else {
+		log.Println("No Update to Repo Notification Sent")
+		err := notifyNoUpdate(owner, repoName)
+		if err != nil {
+			return err
+		}
+		return nil
+
+	}
+
+}
+
+// Returns the last push to the repository requested.
+func callGitHubAPI(client *github.Client, owner string, repoName string) (time.Time, error) {
+	repo, response, err := client.Repositories.Get(context.Background(), owner, repoName)
+	if err != nil {
+		return time.Now(), err
+	}
+	if !((200 <= response.StatusCode) && (response.StatusCode <= 226)) {
+		fmt.Println("Response failed: ", response.StatusCode)
+		return time.Now(), err
+	}
+	return repo.GetPushedAt().Time, nil
+}
+
+// Notification for no update to a repository.
+func notifyNoUpdate(owner string, repoName string) error {
+	err := beeep.Notify(fmt.Sprintf("No update for %s/%s", owner, repoName), fmt.Sprintf("Last Checked: %s", time.Now().Format(time.UnixDate)), "assets/information.png")
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Notification for an update to a repository
+func notifyRepoUpdated(owner string, repoName string, timeOfUpdate time.Time) error {
+	err := beeep.Notify(fmt.Sprintf("%s/%s Updated!", owner, repoName), fmt.Sprintf("Time of Update: %s", timeOfUpdate.Format(time.UnixDate)), "assets/information.png")
+	if err != nil {
+		return err
+	}
+	return nil
+}
 func main() {
 
-	args := os.Args
-	if len(args) < 2 {
-		log.Fatal("USAGE: notify_git_project PATH")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err.Error())
 	}
-	path := args[1]
-	fmt.Println("path", path)
+	GH_TOKEN, tokenIsDefined := os.LookupEnv("GH_TOKEN")
+	if !tokenIsDefined {
+		log.Fatal("Make sure GH_TOKEN is defined in the .env")
+	}
+	owner := "saths008"
+	repoName := "notifyGitProject"
+	client := github.NewClient(nil).WithAuthToken(GH_TOKEN)
+	log.Println("GitHub Client started.")
+	startDaemon(client, owner, repoName)
 }
--- go.mod
+++ go.mod
@@ -3,10 +3,16 @@ module github.com/saths008/notify_git_project
 go 1.22.0
 
 require (
-	github.com/fsnotify/fsnotify v1.7.0 // indirect
-	github.com/gen2brain/beeep v0.0.0-20240112042604-c7bb2cd88fea // indirect
+	github.com/fsnotify/fsnotify v1.7.0
+	github.com/gen2brain/beeep v0.0.0-20240112042604-c7bb2cd88fea
+	github.com/google/go-github/v59 v59.0.0
+)
+
+require (
 	github.com/go-toast/toast v0.0.0-20190211030409-01e6764cf0a4 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
 	github.com/tadvi/systray v0.0.0-20190226123456-11a2b8fa57af // indirect
 	golang.org/x/sys v0.17.0 // indirect
